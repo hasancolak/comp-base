b0b0f6089a6038d589175c7fa3d759d2b130de19
Hasan Colak <hasan.colak@visual-meta.com>
2022-03-25 15:09:20 +0100

    init

--- packages/comp-button/story/comp-button.stories.js
+++ packages/comp-button/story/comp-button.stories.js
@@ -3,7 +3,7 @@ import { Button } from '../src/comp-button';
 
 export default { title: 'Button' };
 
-export const primary = () => <Button>Hello Button</Button>;
+export const primary = () => <Button type="button">Hello Button</Button>;
 
 export const secondary = () => (
   <div style={{ background: '#1652f0', padding: 12 }}>
